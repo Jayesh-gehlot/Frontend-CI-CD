59ad9967e41e819e550bf6bf8e9f8cce97722cda
Jayesh gehlot <jaygehlot99810@gmail.com>
2025-06-29 00:19:45 +0530

    checking ci cd

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ function App() {
       <h1>Hello jayesh</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count  {count}
+          count  hello{count}
         </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
