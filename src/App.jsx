0403210efc20035fe4b2dd4867f78a95ac46ac40
Jayesh gehlot <jaygehlot99810@gmail.com>
2025-06-29 00:25:23 +0530

    checking ci cd 3

--- src/App.jsx
+++ src/App.jsx
@@ -7,27 +7,32 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
+    // <>
+    //   <div>
+    //     <a href="https://vite.dev" target="_blank">
+    //       <img src={viteLogo} className="logo" alt="Vite logo" />
+    //     </a>
+    //     <a href="https://react.dev" target="_blank">
+    //       <img src={reactLogo} className="logo react" alt="React logo" />
+    //     </a>
+    //   </div>
+    //   <h1>Hello jayesh</h1>
+    //   <div className="card">
+    //     <button onClick={() => setCount((count) => count + 1)}>
+    //       count  hello{count}
+    //     </button>
+    //     <p>
+    //       llll <code>src/App.jsx</code> and save to test HMR
+    //     </p>
+    //   </div>
+    //   <p className="read-the-docs">
+    //     Click on the Vite and React logos to learn more
+    //   </p>
+    // </>
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Hello jayesh</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count  hello{count}
-        </button>
-        <p>
-          llll <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    
+    Hello buddy how are you 
+    what going on 
     </>
   )
 }
